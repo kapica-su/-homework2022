90d8546fbaa98ee5a1ac75467c9c19a149e91dd4
kapica-su <97619866+kapica-su@users.noreply.github.com>
2022-05-31 18:47:27 +0700

    Add files via upload

---
+++ day8/part1.py
@@ -0,0 +1,27 @@
+import re
+arraychik = open('input.txt', 'r')
+file = open('output1.txt', 'w+')
+cod = 0
+pamyat = 0
+sebalis = 0
+def memory(string):
+    mem = string[1:-1]
+    mem = mem.replace("\\\\", "x")
+    mem = mem.replace("\\\"", "x")
+    mem, _  = re.subn('\\\\x..', 'x', mem)
+    return len(mem)
+def esc(string):
+    escaped = string
+    escaped = escaped.replace("\\", "\\\\")
+    escaped = escaped.replace('"', '\\"')
+    escaped = '"' + escaped + '"'
+    return len(escaped)
+for line in arraychik:
+    chars = len(line)
+    mem = memory(line)
+    escaped = esc(line)
+    cod += len(line)
+    pamyat += memory(line)
+    sebalis += escaped
+file.write(str(cod - pamyat))
+file.close()
